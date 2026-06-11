7c2a0cc03cc8a9fad69f7de34ef84b4cf04778ca
rawp-id <wpipit35@gmail.com>
2026-06-11 19:39:24 +0700

    Update .gitignore to include /storage/ directory and change return type of index method in HomeController to ViewResponse

--- templates/mvc/app/Controllers/HomeController.php
+++ templates/mvc/app/Controllers/HomeController.php
@@ -5,12 +5,13 @@ namespace App\Controllers;
 use Nexph\Request;
 use Nexph\Route;
 use Nexph\Get;
+use Nexph\View\ViewResponse;
 
 #[Route('/')]
 class HomeController
 {
     #[Get('/')]
-    public function index(): string
+    public function index(): ViewResponse
     {
         return view('home', [
             'title' => 'Welcome to Nexph',
